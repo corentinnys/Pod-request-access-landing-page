86d7057cf461f7c342a1824ecb28d4b5c1371bc0
Corentin <corentinys@gmail.com>
2026-03-16 09:03:53 +0100

    correction js

--- verification.js
+++ verification.js
@@ -1,17 +1,19 @@
 $('.input-group > button').on('click', function(event) {
     event.preventDefault();
    var value =$(this).siblings('input').val();
-
-   if(value.length == 0){
-       $(this).siblings('input').attr('placeholder', 'Please enter your email')
-
-   }
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if(value.length == 0){
 
-    if(regex.test(value)){
-       alert("bien inscrit a notre  newsletter")
-    }else{
-      alert("Email invalide");
-    }
+       alert("Oops! Please add your email")
+   }else{
+
+
+       if(regex.test(value) && value.length > 0){
+           alert("bien inscrit a notre  newsletter")
+       }else{
+           alert("Oops! Please check your email");
+       }
+   }
+
 
 });
